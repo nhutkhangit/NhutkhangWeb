8f069dc2c0b489e1a14de92c525f7b67bc9c35c2
Liên Nhựt Khang <kendy.lien@beyondedge.co>
2019-10-17 12:54:33 +0700

    commit index.php

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Nhựt Khang Web</title>
+</head>
+<body>
+    <?php
+        for ($i=0; $i < 4; $i++) {
+            ?>
+            <div>Test php</div>
+            <?php
+        }
+    ?>
+</body>
+</html>
